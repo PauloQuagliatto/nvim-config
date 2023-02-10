77b10e1a2498bbdaab9c568731c2988208083912
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-02-10 09:10:50 -0300

    highlights rgb in javascript, typescript and html

--- after/plugin/colorizer.lua
+++ after/plugin/colorizer.lua
@@ -1,5 +1,14 @@
 require 'colorizer'.setup({
     '*',
+    typescript = {
+        css = true
+    },
+    javascript = {
+        css = true
+    },
+    html = {
+        css = true
+    },
     css = {
         css = true,
         css_fn = true
