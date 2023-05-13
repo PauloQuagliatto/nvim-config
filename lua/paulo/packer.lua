cced6841d21fa78f7556d7586492b2551d5b1409
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-05-13 18:18:19 -0300

    added nvimtree

--- lua/paulo/packer.lua
+++ lua/paulo/packer.lua
@@ -19,6 +19,16 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end
+}
+
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/playground')
   use('theprimeagen/harpoon')
