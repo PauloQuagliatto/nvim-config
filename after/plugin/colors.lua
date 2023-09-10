185a8f80eb90004b3a8365c60d1d7a932105f59f
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-09-10 15:06:51 -0300

    new config is lit

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,9 +2,8 @@ function ColorMyEditor(color)
 	color = color or "onenord"
 	vim.cmd.colorscheme(color)
 
-  
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyEditor()
--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -3,14 +3,63 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 25,
+    adaptive_size = true,
   },
   renderer = {
     group_empty = true,
+    indent_markers = {
+      enable = true,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        item = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = false,
+      show = {
+        file = false,
+        folder = true,
+        folder_arrow = false,
+        git = true
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "", -- 
+          staged = "",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
   },
   filters = {
     dotfiles = false
   },
   diagnostics = {
-    enable = true
+    enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
   }
 })
