350a8e7d7263750c99f7771168120426ba2b1ef1
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-11-20 16:29:26 -0300

    finished my current setup

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,3 +1,2 @@
-vim.keymap.set("n", "<C-t>", vim.cmd.FloatermToggle, opts, { remap = true })
-vim.keymap.set("t", "<C-t>", vim.cmd.FloatermToggle, opts, { remap = true })
---vim.keymap.set("ntn", "<C-t>", vim.cmd.FloatermToggle, opts, { remap = true })
+vim.keymap.set("n", "<C-t>", vim.cmd.FloatermToggle, { remap = true })
+vim.keymap.set("t", "<C-t>", vim.cmd.FloatermToggle, { remap = true })
