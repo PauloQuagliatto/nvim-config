2ba22b2826da59e38dfca5f2bb727a0b6f585f76
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-11-29 16:59:27 -0300

    ah

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,13 +23,10 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert(cmp_mappings)
 })
 
-lsp.set_preferences({
-  sign_icons = {}
-})
-
 lsp.on_attach(function(client, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
+  vim.keymap.set('n', 'gl', function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,7 +1,8 @@
-require('telescope').setup{ defaults = { file_ignore_patterns = {"node_modules"} } }
+require('telescope').setup { defaults = { file_ignore_patterns = { "node_modules" } } }
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>s', function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+--vim.keymap.set('n', '<leader>rg', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
