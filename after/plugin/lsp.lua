bc1c05292ae3419af499a59d8c58e5983a9d0473
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-12-01 11:55:40 -0300

    created a better solution

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,8 +7,22 @@ require('mason-lspconfig').setup({
   ensure_installed = { 'tsserver', 'rust_analyzer', 'lua_ls' },
   handlers = {
     lsp.default_setup,
-  },
+    load(function()
+      if require("lspconfig").tsserver then
+        vim.keymap.set("n", "<leader>of", function()
+          vim.lsp.buf.execute_command({
+            command = "_typescript.organizeImports",
+            arguments = { vim.api.nvim_buf_get_name(0) },
+            title = ""
+          })
+          vim.cmd.EslintFixAll()
+        end)
+      end
+    end
+    )
+  }
 })
+
 local cmp = require('cmp')
 local cmp_format = lsp.cmp_format()
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -37,16 +51,6 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set("n", "<leader>of", function()
-    if require("lspconfig").tsserver then
-      vim.lsp.buf.execute_command({
-        command = "_typescript.organizeImports",
-        arguments = { vim.api.nvim_buf_get_name(0) },
-        title = ""
-      })
-      vim.cmd.EslintFixAll()
-    end
-  end)
 end)
 
 lsp.setup()
