f3a080240c01157c8f41e6139ec654c9b741701d
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2024-05-25 09:44:52 -0300

    new additions

--- lua/paulo/remap.lua
+++ lua/paulo/remap.lua
@@ -9,16 +9,13 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
@@ -32,6 +29,12 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>lsr", "<cmd>:LspRestart<CR>", { remap = true })
+
+-- Tab walk remap
+vim.keymap.set("n", "<leader>te", "<cmd>:tabnew<CR>")
+vim.keymap.set("n", "<leader>tn", "<cmd>:tabnext<CR>")
+vim.keymap.set("n", "<leader>tp", "<cmd>:tabprev<CR>")
+vim.keymap.set("n", "<leader>tc", "<cmd>:tabclose<CR>")
 
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(e)
